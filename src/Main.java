2e5411a255aa228a6f5eb1bd80b0d60d76e5d332
RAGAVI2904 <ragavia1999@gmail.com>
2024-03-10 16:27:07 +0530

    Just pushing everything

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void m1(){
+        System.out.println("M1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        m1();
+    }
+}
